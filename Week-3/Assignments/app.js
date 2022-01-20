1eb6089b71370a5a0446864e5e09ddc2c5eca1a8
hsintzuli <hsintzuli719@gmail.com>
2022-01-20 08:34:41 +0800

    Finish week-3 assignment1, set up

---
+++ Week-3/Assignments/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello, My Server!</h1>");
+})
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!');
+});
