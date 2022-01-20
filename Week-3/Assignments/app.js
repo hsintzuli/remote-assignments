ca36f8ec3a6b783d768e76fade4b63a0be5f1402
hsintzuli <hsintzuli719@gmail.com>
2022-01-20 10:00:43 +0800

    Finish week-3 assignment2, calculate sum

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -4,8 +4,30 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello, My Server!</h1>");
+    res.send('<h1>Hello, My Server!</h1>');
 })
+
+app.get('/data', (req, res) => {
+    let { number } = req.query;
+    if ( !number ) {
+        throw new Error("Lack of Parameter");
+    }
+
+    number = parseInt(number);
+    if (isNaN(number) || number <= 0) {
+        throw new Error("Wrong Parameter");
+    }
+
+    const ans = number * (number + 1) / 2;
+    res.render('data', { number, ans } );
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    console.log(`Error: ${err.message}`);
+    res.render('error');
+});
+
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!');
 });
